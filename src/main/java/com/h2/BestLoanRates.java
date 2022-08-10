1754e5e2f2c87a7f330349c3b001d2b0d9e09578
Adelaide Craig <99053726+ayyyecraig@users.noreply.github.com>
2022-08-10 12:29:34 -0500

    completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -5,36 +5,35 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class BestLoanRates {
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
+
         System.out.println("Enter the loan term (in years)");
         int loanTermInYears = scanner.nextInt();
-        getRates(loanTermInYears);
+        float bestRates = getRates(loanTermInYears);
+        if(bestRates == 0.0f){
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
+        } else {
+            System.out.println("Best Available Rate: " + bestRates + "%");
+        }
 
 
     }
-    public static final Map<Integer, Float> bestRates;
 
-    static {
-        bestRates = new HashMap<>();
-        bestRates.put(1, 5.50f);
-        bestRates.put(2, 3.45f);
-        bestRates.put(3, 2.67f);
-    }
 
    public static float getRates(int loanTermInYears) {
-      if (loanTermInYears == 1){
-          return Float.parseFloat("best rate for '1 year' must be '5.50f'");
-    } else if(loanTermInYears == 2){
-          return Float.parseFloat("best rate for '2 year' must be '3.45f'");
-      }else if(loanTermInYears == 3){
-          return Float.parseFloat("best rate for '3 year' must be '2.67f'");
-      }else {
-          return Float.parseFloat("best rate for '100 year' must be '0.0ff'");
-      }
+     if(bestRates.containsKey(loanTermInYears)){
+         return bestRates.get(loanTermInYears);
+
+     }return 0.0f;
 
       
 
